f942267bcfa632ca2bf7c8e11ec2cbbc94ba86fa
Alline Marjorie <allinemarjorie@gmail.com>
2020-06-30 22:23:17 -0300

    refactor: rewrite index.js to require and remove get

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import express from "express";
-// const express = require("express");
-
+const express = require("express");
 const app = express();
 
 app.listen(3000, () => console.log("servidor rodando em localhost:3000 woo"));
-
-app.get("/", (req, res) => {
-  res.send("primeira rota express woo");
-});
